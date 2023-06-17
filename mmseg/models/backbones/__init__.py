a969dd94fb1daa83f79d60f576c28d643baa2cf8
Matthias Zeller <m.zeller1901@gmail.com>
2023-06-17 16:33:10 -0400

    migrate cswin to mmsegv2

--- mmseg/models/backbones/__init__.py
+++ mmseg/models/backbones/__init__.py
@@ -18,6 +18,7 @@ from .resnet import ResNet, ResNetV1c, ResNetV1d
 from .resnext import ResNeXt
 from .stdc import STDCContextPathNet, STDCNet
 from .swin import SwinTransformer
+from .cswin import CSWinTransformer
 from .timm_backbone import TIMMBackbone
 from .twins import PCPVT, SVT
 from .unet import UNet
@@ -28,5 +29,6 @@ __all__ = [
     'ResNeSt', 'MobileNetV2', 'UNet', 'CGNet', 'MobileNetV3',
     'VisionTransformer', 'SwinTransformer', 'MixVisionTransformer',
     'BiSeNetV1', 'BiSeNetV2', 'ICNet', 'TIMMBackbone', 'ERFNet', 'PCPVT',
-    'SVT', 'STDCNet', 'STDCContextPathNet', 'BEiT', 'MAE', 'PIDNet', 'MSCAN'
+    'SVT', 'STDCNet', 'STDCContextPathNet', 'BEiT', 'MAE', 'PIDNet', 'MSCAN',
+    'CSWinTransformer'
 ]
