d19e1683027bc7d9f0402a91c843c447b993d96a
Matthias Zeller <m.zeller1901@gmail.com>
2023-06-22 19:12:21 -0400

    fix

--- mmseg/evaluation/metrics/class_iou_metric.py
+++ mmseg/evaluation/metrics/class_iou_metric.py
@@ -1,10 +1,11 @@
 # Modified by Matthias Gilles Zeller
 import os.path as osp
-from typing import Dict, List, Optional, Sequence
+from collections import OrderedDict
+from typing import Dict, List, Optional
 
 import numpy as np
-from PIL import Image
 from mmengine.dist import is_main_process
+from mmengine.logging import MMLogger, print_log
 from mmengine.utils import mkdir_or_exist
 from prettytable import PrettyTable
 
@@ -61,40 +62,67 @@ class ClassIoUMetric(IoUMetric):
             mkdir_or_exist(self.output_dir)
         self.format_only = format_only
 
-    def process(self, data_batch: dict, data_samples: Sequence[dict]) -> None:
-        """Process one batch of data and data_samples.
-
-        The processed results should be stored in ``self.results``, which will
-        be used to compute the metrics when all batches have been processed.
+    def compute_metrics(self, results: list) -> Dict[str, float]:
+        """Compute the metrics from processed results.
 
         Args:
-            data_batch (dict): A batch of data from the dataloader.
-            data_samples (Sequence[dict]): A batch of outputs from the model.
+            results (list): The processed results of each batch.
+
+        Returns:
+            Dict[str, float]: The computed metrics. The keys are the names of
+                the metrics, and the values are corresponding results. The key
+                mainly includes aAcc, mIoU, mAcc, mDice, mFscore, mPrecision,
+                mRecall.
         """
-        num_classes = len(self.dataset_meta['classes'])
-        for data_sample in data_samples:
-            pred_label = data_sample['pred_sem_seg']['data'].squeeze()
-            # format_only always for test dataset without ground truth
-            if not self.format_only:
-                label = data_sample['gt_sem_seg']['data'].squeeze().to(
-                    pred_label)
-                self.results.append(
-                    self.intersect_and_union(pred_label, label, num_classes,
-                                             self.ignore_index))
-            # format_result
-            if self.output_dir is not None:
-                basename = osp.splitext(osp.basename(
-                    data_sample['img_path']))[0]
-                png_filename = osp.abspath(
-                    osp.join(self.output_dir, f'{basename}.png'))
-                output_mask = pred_label.cpu().numpy()
-                # The index range of official ADE20k dataset is from 0 to 150.
-                # But the index range of output is from 0 to 149.
-                # That is because we set reduce_zero_label=True.
-                if data_sample.get('reduce_zero_label', False):
-                    output_mask = output_mask + 1
-                output = Image.fromarray(output_mask.astype(np.uint8))
-                output.save(png_filename)
+        logger: MMLogger = MMLogger.get_current_instance()
+        if self.format_only:
+            logger.info(f'results are saved to {osp.dirname(self.output_dir)}')
+            return OrderedDict()
+        # convert list of tuples to tuple of lists, e.g.
+        # [(A_1, B_1, C_1, D_1), ...,  (A_n, B_n, C_n, D_n)] to
+        # ([A_1, ..., A_n], ..., [D_1, ..., D_n])
+        results = tuple(zip(*results))
+        assert len(results) == 4
+
+        total_area_intersect = sum(results[0])
+        total_area_union = sum(results[1])
+        total_area_pred_label = sum(results[2])
+        total_area_label = sum(results[3])
+        ret_metrics = self.total_area_to_metrics(
+            total_area_intersect, total_area_union, total_area_pred_label,
+            total_area_label, self.metrics, self.nan_to_num, self.beta)
+
+        class_names = self.dataset_meta['classes']
+
+        # summary table
+        ret_metrics_summary = OrderedDict({
+            ret_metric: np.round(np.nanmean(ret_metric_value) * 100, 2)
+            for ret_metric, ret_metric_value in ret_metrics.items()
+        })
+        metrics = dict()
+        for key, val in ret_metrics_summary.items():
+            if key == 'aAcc':
+                metrics[key] = val
+            else:
+                metrics['m' + key] = val
+
+        # each class table
+        ret_metrics.pop('aAcc', None)
+        ret_metrics_class = OrderedDict({
+            ret_metric: np.round(ret_metric_value * 100, 2)
+            for ret_metric, ret_metric_value in ret_metrics.items()
+        })
+        ret_metrics_class.update({'Class': class_names})
+        ret_metrics_class.move_to_end('Class', last=False)
+        class_table_data = PrettyTable()
+        for key, val in ret_metrics_class.items():
+            class_table_data.add_column(key, val)
+
+        print_log('per class results:', logger)
+        print_log('\n' + class_table_data.get_string(), logger=logger)
+
+        metrics.update(self._get_class_metrics(class_table_data))
+        return metrics
 
     @staticmethod
     def _get_class_metrics(table: PrettyTable) -> Dict[str, float]:
