d0ec16ff7a650c256198f802713a911e46548f0a
Matthias Zeller <m.zeller1901@gmail.com>
2023-06-22 11:47:41 -0400

    new hook: data split

--- mmseg/engine/hooks/__init__.py
+++ mmseg/engine/hooks/__init__.py
@@ -1,4 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .visualization_hook import SegVisualizationHook
+from .data_split_hook import DataSplitHook
 
-__all__ = ['SegVisualizationHook']
+__all__ = ['SegVisualizationHook', 'DataSplitHook']
+
---
+++ mmseg/engine/hooks/data_split_hook.py
@@ -0,0 +1,55 @@
+
+from pathlib import Path
+import json
+from logging import WARNING
+from itertools import combinations
+
+from mmengine.hooks import Hook
+from mmengine.runner import Runner
+from mmengine.logging import print_log
+
+from mmseg.registry import HOOKS
+
+
+@HOOKS.register_module()
+class DataSplitHook(Hook):
+
+    def __init__(self, assert_no_overlap: bool = True, log_split: bool = True):
+        self.assert_no_overlap = assert_no_overlap
+        self.log_split = log_split
+
+    def before_run(self, runner: Runner) -> None:
+        dset_val = runner.val_dataloader.dataset
+        dset_train = runner.train_dataloader.dataset
+        dset_test = runner.test_dataloader.dataset
+
+        info = dict()
+        for dset, desc in [(dset_train, 'train'), (dset_val, 'val'), (dset_test, 'test')]:
+            buffer = list()
+            for idx in range(len(dset)):
+                dic = dset.get_data_info(idx)
+                dic = {
+                    'img_name': Path(dic['img_path']).name,
+                    'seg_map_name': Path(dic['seg_map_path']).name,
+                    **dic
+                }
+                buffer.append(dic)
+            info[desc] = buffer
+
+        if self.assert_no_overlap:
+            names = {
+                setname: set(e['img_path'] for e in lst)
+                for setname, lst in info.items()
+            }
+
+            for a, b in combinations(names.keys(), 2):
+                intersection = names[a].intersection(names[b])
+                if len(intersection) > 0:
+                    print_log(f'overlapping samples between {a} and {b}: {intersection}', logger='current', level=WARNING)
+
+
+        if self.log_split:
+            logdir = Path(runner.log_dir)
+            with logdir.joinpath('data.json').open('wt') as f:
+                json.dump(info, f, indent=4)
+
