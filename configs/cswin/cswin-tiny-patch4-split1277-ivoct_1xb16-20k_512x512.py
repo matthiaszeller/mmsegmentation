5cbfd09d5e8eec86040ad33f819110e01de9c16d
Matthias Zeller <m.zeller1901@gmail.com>
2023-06-22 19:38:45 -0400

    config

--- configs/cswin/cswin-tiny-patch4-split1277-ivoct_1xb16-20k_512x512.py
+++ configs/cswin/cswin-tiny-patch4-split1277-ivoct_1xb16-20k_512x512.py
@@ -56,7 +56,9 @@ train_dataloader = dict(batch_size=16)
 val_dataloader = dict(batch_size=1)
 test_dataloader = val_dataloader
 
-# Adjust logging interval
 default_hooks = dict(
+    # Adjust logging interval
     logger=dict(interval=10),
+    # Checkpointing
+    checkpoint=dict(save_best='mIoU', rule='greater'),
 )
