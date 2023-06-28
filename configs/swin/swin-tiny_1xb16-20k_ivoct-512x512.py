ffe900ffb8d617d88dfad594a8721b597c9b502b
Matthias Zeller <m.zeller1901@gmail.com>
2023-06-27 21:45:52 -0400

    config swin scratch

---
+++ configs/swin/swin-tiny_1xb16-20k_ivoct-512x512.py
@@ -0,0 +1,63 @@
+_base_ = [
+    '../_base_/models/upernet_swin.py', '../_base_/datasets/ivoct.py',
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_20k.py'
+]
+crop_size = (512, 512)
+data_preprocessor = dict(size=crop_size)
+
+model = dict(
+    data_preprocessor=data_preprocessor,
+    backbone=dict(
+        init_cfg=None,
+        embed_dims=96,
+        depths=[2, 2, 6, 2],
+        num_heads=[3, 6, 12, 24],
+        window_size=7,
+        use_abs_pos_embed=False,
+        drop_path_rate=0.3,
+        patch_norm=True),
+    decode_head=dict(in_channels=[96, 192, 384, 768], num_classes=150),
+    auxiliary_head=dict(in_channels=384, num_classes=150))
+
+# AdamW optimizer, no weight decay for position embedding & layer norm
+# in backbone
+optim_wrapper = dict(
+    _delete_=True,
+    type='OptimWrapper',
+    optimizer=dict(
+        type='AdamW', lr=0.00006, betas=(0.9, 0.999), weight_decay=0.01),
+    paramwise_cfg=dict(
+        custom_keys={
+            'absolute_pos_embed': dict(decay_mult=0.),
+            'relative_position_bias_table': dict(decay_mult=0.),
+            'norm': dict(decay_mult=0.)
+        }))
+
+
+_warmup_iter = 180
+param_scheduler = [
+    dict(
+        type='LinearLR', start_factor=1e-6, by_epoch=False, begin=0, end=_warmup_iter),
+    dict(
+        type='PolyLR',
+        eta_min=0.0,
+        power=1.0,
+        begin=_warmup_iter,
+        end=20000,
+        by_epoch=False,
+    )
+]
+
+
+train_dataloader = dict(batch_size=16)
+val_dataloader = dict(batch_size=1)
+test_dataloader = val_dataloader
+
+default_hooks = dict(
+    # Adjust logging interval
+    logger=dict(interval=20),
+    # Checkpointing
+    checkpoint=dict(save_best='metric/mIoU', rule='greater', max_keep_ckpts=10),
+)
+# Evaluate more often, takes few seconds only in this config
+train_cfg = dict(val_interval=200)
