6074f5c19b38e3424b3c8e0634c66f44958fda16
Matthias Zeller <mazeller@mit.edu>
2023-06-27 20:29:24 -0400

    IVOCT config: with and without data aug

--- configs/_base_/datasets/ivoct_aug.py
+++ configs/_base_/datasets/ivoct_aug.py
@@ -8,11 +8,10 @@ train_pipeline = [
     # note: loading relies on mmcv.frombytes, which has BGR and not RGB
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations'),
-    dict(type='Resize', scale=(512, 512), keep_ratio=True),
-    #dict(type='RandomResize', scale=(512, 512), ratio_range=(0.5, 2.0), keep_ratio=True),
-    #dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
+    dict(type='RandomResize', scale=(512, 512), ratio_range=(0.5, 2.0), keep_ratio=True),
+    dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
     dict(type='RandomFlip', prob=0.5),
-    #dict(type='PhotoMetricDistortion'),
+    dict(type='PhotoMetricDistortion'),
     dict(type='PackSegInputs')
 ]
 
