5ea8ee87391b6deca500a3e6ae5718330e0bc8fd
Matthias Zeller <m.zeller1901@gmail.com>
2023-06-30 16:27:11 -0400

    SampleIoUMetric

--- mmseg/evaluation/metrics/sample_iou_metric.py
+++ mmseg/evaluation/metrics/sample_iou_metric.py
@@ -21,24 +21,27 @@ class SampleIoUMetric(BaseMetric):
                  collect_device: str = 'cpu',
                  prefix: Optional[str] = None):
         super().__init__(collect_device=collect_device, prefix=prefix)
+        self.labels = None
+
+    def _get_sample_metrics(self, pred_label, label):
+        res = dict()
+        for name, fun in [('IoU', jaccard_score), ('Precision', precision_score),
+                          ('Recall', recall_score), ('F1', f1_score)]:
+            buffer = fun(label, pred_label, average=None, zero_division=0, labels=self.labels)
+            for value, class_name in zip(buffer, self.dataset_meta['classes']):
+                res[f'{name}.{class_name}'] = value
+
+        return res
 
     def process(self, data_batch: dict, data_samples: Sequence[dict]) -> None:
-        def get_sample_metrics(pred_label, label):
-            res = dict()
-            for name, fun in [('IoU', jaccard_score), ('Precision', precision_score),
-                              ('Recall', recall_score), ('F1', f1_score)]:
-                buffer = fun(label, pred_label, average=None, zero_division=0, labels=labels)
-                for value, class_name in zip(buffer, self.dataset_meta['classes']):
-                    res[f'{name}.{class_name}'] = value
-
-            return res
-
-        labels = np.arange(len(self.dataset_meta['classes']))
-        for data_sample in data_samples:
-            pred_label = data_sample['pred_sem_seg']['data'].squeeze()
-            label = data_sample['gt_sem_seg']['data'].squeeze().to(pred_label)
-
-            self.results.append(get_sample_metrics(pred_label.view(-1), label.view(-1)))
+        self.labels = np.arange(len(self.dataset_meta['classes']))
+        for ds in data_samples:
+            pred = ds['pred_sem_seg']['data'].squeeze().cpu().view(-1)
+            label = ds['gt_sem_seg']['data'].squeeze().cpu().to(pred).view(-1)
+            self.results.append(self._get_sample_metrics(pred, label))
 
     def compute_metrics(self, results: list) -> dict:
-        raise NotImplementedError('dont call this metric during training or validation')
+        return {
+            'samples': results
+        }
+
