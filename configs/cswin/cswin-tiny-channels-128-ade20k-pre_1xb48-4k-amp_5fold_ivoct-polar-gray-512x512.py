14a2eb17c1cea409187a510006cf4bbfb8c68b03
Matthias Zeller <mazeller@mit.edu>
2023-08-11 19:47:42 -0400

    cswin config 128 channels

---
+++ configs/cswin/cswin-tiny-channels-128-ade20k-pre_1xb48-4k-amp_5fold_ivoct-polar-gray-512x512.py
@@ -0,0 +1,51 @@
+_base_ = [
+    './cswin-tiny-ade20k-pre_1xb16-20k_ivoct-polar-gray-512x512.py'
+]
+
+model = dict(
+    init_cfg=dict(type='Pretrained', checkpoint=None),
+    decode_head=dict(
+        channels=128
+    ),
+    auxiliary_head=dict(
+        channels=128
+    )
+)
+
+train_dataloader=dict(
+    batch_size=48,
+    dataset=dict(
+        # use --cfg-options to replace this argument with the correct fold id
+        ann_file='splits/segmentation/train_fold<i>.txt'
+    )
+)
+
+val_dataloader=dict(
+    dataset=dict(
+        # use --cfg-options to replace this argument with the correct fold id
+        ann_file='splits/segmentation/val_fold<i>.txt'
+    )
+)
+
+test_dataloader=val_dataloader
+
+
+optim_wrapper = dict(
+    optimizer=dict(lr=0.00006 * 4),
+)
+
+# 4k iterations
+train_cfg = dict(max_iters=4000, val_interval=100)
+warmup_iters = 45
+param_scheduler = [
+    dict(
+        type='LinearLR', start_factor=1e-6, by_epoch=False, begin=0, end=warmup_iters),
+    dict(
+        type='PolyLR',
+        eta_min=1e-8,
+        power=1.0,
+        begin=warmup_iters,
+        end=4000,
+        by_epoch=False,
+    )
+]
